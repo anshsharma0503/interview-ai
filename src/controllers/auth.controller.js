c6d3cf431f96647e6d197304c8a8d8c790cec8f0
AnshSharma-05 <sharmaji.ansh05032005@gmail.com>
2026-03-21 12:05:58 +0530

    Fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,47 +8,47 @@ const jwt = require("jsonwebtoken")
  * @access public
  */
 
-async function registerUserController(req , res) {
-    const { username , email , password } = req.body
+async function registerUserController(req, res) {
+    const { username, email, password } = req.body
 
-    if(!username || !email || !password) {
+    if (!username || !email || !password) {
         return res.status(400).json({
-            message : "plaese provide username , email and password"
+            message: "plaese provide username , email and password"
         })
     }
 
     const isUserAlreadyExists = await userModel.findOne({
-        $or : [{ username },{ email }]
+        $or: [{ username }, { email }]
     })
 
-    if(isUserAlreadyExists){
+    if (isUserAlreadyExists) {
         return res.status(400).json({
             message: "Account alredy exists with this username or email address"
         })
     }
 
-    const hash = await bcrypt.hash(password , 10)
+    const hash = await bcrypt.hash(password, 10)
 
     const user = await userModel.create({
         username,
         email,
-        password: hash   
+        password: hash
     })
 
     const token = jwt.sign(
-        {id: user._id , username: user.username},
+        { id: user._id, username: user.username },
         process.env.JWT_SECRET,
-        { expiresIn: "1d"}
+        { expiresIn: "1d" }
     )
 
-    res.cookie("token",token)
+    res.cookie("token", token)
 
     res.status(201).json({
         message: "User registered successfully",
         user: {
             id: user._id,
             username: user.username,
-            email: user.email        
+            email: user.email
         }
     })
 }
@@ -59,32 +59,32 @@ async function registerUserController(req , res) {
  * @access Public
  */
 
-async function loginUserController(req , res){
-    const { email , password} = req.body
+async function loginUserController(req, res) {
+    const { email, password } = req.body
 
     const user = await userModel.findOne({ email })
 
-    if(!user) {
+    if (!user) {
         return res.status(400).json({
             message: "Invalid email or password"
         })
     }
 
-    const isPasswordValid = await bcrypt.compare(password , user.password)
+    const isPasswordValid = await bcrypt.compare(password, user.password)
 
-    if(!isPasswordValid){
+    if (!isPasswordValid) {
         return res.status(400).json({
             message: "Invalid email or password"
         })
     }
 
     const token = jwt.sign(
-        {id: user._id , username: user.username},
+        { id: user._id, username: user.username },
         process.env.JWT_SECRET,
-        { expiresIn: "1d"}
+        { expiresIn: "1d" }
     )
 
-    res.cookie("token" , token)
+    res.cookie("token", token)
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
